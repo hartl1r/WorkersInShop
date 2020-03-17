802cc02474ff5c11e7696ac6fd4f58d03e7b2486
hartl1r <hartl1r@gmail.com>
2020-03-17 14:41:20 -0400

    Tues 2:41
    Signed-off-by: hartl1r <hartl1r@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -14,155 +14,61 @@ from datetime import date
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("checkin.html")
-
-@app.route('/checkIn', methods=["GET","POST"])
-def checkIn():
-    shopNumber = "1"
-
-    if request.method != 'POST':
-        return
-    requestData = request.get_json()
-    villageID = requestData.get("memberID")
-    typeOfWorkOverride = requestData.get("typeOfWork")
+    shop = request.cookies.get('shopAbbr')
+    # IF NO COOKIE IS FOUND, PROMPT FOR SHOP ABBREVIATION
+    if shop == None:
+        return render_template("index.html")
+        #return render_template("index.html")
+    # IF A COOKIE IS FOUND, SAVE THE SHOP ABBR AND OPEN THE WORKERSINSHOP PAGE
+    if shop == 'RA':
+        shopName = 'Rolling Acres'
+    elif shop == 'BW':
+        shopName = 'Brownwood'
     
-    # Look up member ID
-    sqlSelect = """ SELECT Member_ID, First_Name, Last_Name, NonMember_Volunteer, Certified, Certified_2,Default_Type_Of_Work, Restricted_From_Shop, Reason_For_Restricted_From_Shop, noteToMember
-        FROM tblMember_Data LEFT JOIN notesToMembers ON tblMember_Data.Member_ID = notesToMembers.memberID
-        WHERE tblMember_Data.Member_ID='""" + villageID + """'"""
-    
-    member = db.engine.execute(sqlSelect)
-    row = 0
-    for m in member:
-        row += 1
-        villageID = m.Member_ID
-        memberName = m.First_Name + " " + m.Last_Name
-        typeOfWorkToUse = "General"
-        if (m.Default_Type_Of_Work != None):
-            typeOfWorkToUse = m.Default_Type_Of_Work
+    # SAVE SHOP ABBREVIATION IN A SESSION VARIABLE ???
+    return redirect (url_for('workersInShop',shop=shop))
+  #  return redirect(url_for('trainingClass',id=trainingClassID))
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        shopAbbrInput = request.form['shopAbbr']
+        resp = make_response('Setting cookie for current shop.')
+        resp.set_cookie('shopAbbr', shopAbbrInput)
+        # SAVE SHOP ABBREVIATION IN A SESSION VARIABLE ???
+        return redirect (url_for('workersInShop',shop=shop))
+
+
+@app.route('/getcookie')
+def getcookie():
+    shop = request.cookies.get('shopAbbr')
+    if shop == 'RA':
+        shopName = 'Rolling Acres'
+    elif shop == 'BW':
+        shopName = 'Brownwood'
+    else:
+        shopName = 'Not specified'
         
-        if (typeOfWorkOverride != "General"):
-            typeOfWorkToUse = typeOfWorkOverride
+    return '<h1>Welcome to ' +shopName+'</h1>'      
 
-        certified1 = m.Certified
-        certified2 = m.Certified_2
-        restricted = m.Restricted_From_Shop
-        reasonRestricted = m.Reason_For_Restricted_From_Shop
-        volunteer = m.NonMember_Volunteer
+@app.route("/workersInShop/<string:shop>/",methods=['GET','POST'])
+def workersInShop(shop):
+    todaysDate = date.today()
 
-    # Were any records found?
-    if row == 0:
-        response_body = {
-        "status" :"Not Found"
-        }
-        res = make_response(jsonify(response_body),200)
-        return(res)
+    sqlCheckInRecord = """SELECT (Last_Name + ', ' +  First_Name) as memberName, tblMember_Activity.Member_ID,
+     format(Check_In_Date_Time,'hh:mm tt') as CheckInTime, Format(Check_Out_Date_Time,'hh:mm tt') as CheckOutTime,
+                Type_Of_Work, Emerg_Name, Emerg_Phone, Shop_Number, Door_Used, isMentor
+            FROM tblMember_Activity left join tblMember_Data on tblMember_Activity.Member_ID = tblMember_Data.Member_ID 
+            WHERE Check_Out_Date_Time Is Null 
+            AND Cast(Check_In_Date_Time as DATE) >= '""" + str(todaysDate) + """'"""
+    print(sqlCheckInRecord)
+    workersInShop = db.engine.execute(sqlCheckInRecord)
+    
+#     for w in workersInShop:
+#         #recordID = w.ID
+#         typeOfWorkAtCheckIn = w.Type_Of_Work
+#         checkInTime = w.Check_In_Date_Time
+#         memberCheckedIn = True
+#         print (w.memberName, w.Check_In_Date_Time, w.Check_Out_Date_Time, w.Emergency_Contact)            
         
-    # Member record was found    
-    
-    # Is member restricted?
-    if restricted:
-        response_body = {
-            "status": "Restricted",
-            "memberName": memberName,
-            "reason": reasonRestricted
-        }
-        res = make_response(jsonify(response_body),200) 
-        return(res)
-
-    # Is member certified?
-    if shopNumber == '1' and not certified1 :
-        response_body = {
-        "status" :"Not Certified",
-        "msg": "Not certified for ROLLING ACRES"
-        }
-        res = make_response(jsonify(response_body),200)
-        return(res)
-           
-    if shopNumber == '2' and not certified2 :
-        response_body = {
-        "status" :"Not Certified",
-        "msg": "Not certified for BROWNWOOD"
-        }
-        res = make_response(jsonify(response_body),200)
-        return(res)
-    
-    # Member, or volunteer is not restricted so may be checked in/out
-    if not restricted:
-        # Retrieve today's check in record for this member
-        #sqlActivity = """SELECT Type_Of_Work FROM tblMember_Activity WHERE memberID = '"" + villageID + '''"""
-        sqlCheckInRecord = """SELECT ID, Member_ID, Check_In_Date_Time, Check_Out_Date_Time, Type_Of_Work
-            FROM tblMember_Activity 
-            WHERE Member_ID = '""" + villageID + """' AND Check_Out_Date_Time Is Null 
-            AND Check_In_Date_Time >= CAST (GETDATE() as DATE) """
-            #AND Format(Check_In_Date_Time,'mm/dd/yyyy') >= Format(CAST (GETDATE() as DATE),'mm/dd/yyyy') """  
-
-       # Look for current checkin in the table tblMember_Activity
-        memberCheckedIn = False 
-        typeOfWorkAtCheckIn=""
-        activity = db.engine.execute(sqlCheckInRecord)
-        for a in activity:
-            recordID = a.ID
-            typeOfWorkAtCheckIn = a.Type_Of_Work
-            checkInTime = a.Check_In_Date_Time
-            memberCheckedIn = True
-
-        #Is member checked in?
-        if not memberCheckedIn:
-            processCheckIn(villageID,typeOfWorkToUse,shopNumber)
-            response_body = {
-                "status": "Check In",
-                "memberName": memberName,
-                "checkInTime":datetime.datetime.now().strftime('%I:%M %p'),
-                "typeOfWork": typeOfWorkToUse
-            }
-            res = make_response(jsonify(response_body),200)
-            return(res)
-        else:
-            processCheckOut(recordID)
-            response_body = {
-                "status": "Check Out",
-                "memberName": memberName,
-                "checkInTime": checkInTime.strftime('%I:%M %p'),
-                "checkOutTime":datetime.datetime.now().strftime('%I:%M %p'),
-                "typeOfWork": typeOfWorkAtCheckIn
-            }
-            res = make_response(jsonify(response_body),200)
-            return(res)
-
-    # If no condition is met return the following -
-    response_body = {
-        "status" :"Error"
-    }
-    res = make_response(jsonify(response_body),200)
-    return(res)
-
-def processCheckIn(villageID,typeOfWork,shopNumber):
-    checkInDateTime = datetime.datetime.now().strftime("%d/%m/%y %I:%M %p")
-    #sqlInsert = """INSERT INTO tblMember_Activity (Member_ID,Check_In_Date_Time,Type_Of_Work,Door_Used,Shop_Number) VALUES ('"""
-    #+ villageID + """', '""" + checkInDateTime + """', '""" + typeOfWork + """','FRONT', """ + str(shopNumber) + """)"""
-    #print (sqlInsert)   
-    try:
-        activity = MemberActivity(Member_ID=villageID,Check_In_Date_Time=checkInDateTime,Type_Of_Work=typeOfWork,Shop_Number=int(shopNumber),Door_Used='Front')
-        db.session.add(activity)
-        db.session.commit()
-        flash("Check in added successfully.","success")
-        return 
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        db.session.rollback()
-        return 
-    return
-
-def processCheckOut(recordID):
-    checkOutDateTime = date.today().strftime("%d/%m/%y %I:%M %p")
-    try:
-        activity = db.session.query(MemberActivity).filter(MemberActivity.ID == recordID).one()
-        activity.Check_Out_Date_Time = checkOutDateTime
-        db.session.commit()
-        return
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        db.session.rollback()
-        flash("Check out could not be completed.\n"+error,"danger")
-        return
+    return render_template("workersInShop.html",workersInShop=workersInShop,shop=shop)
---
+++ app/static/js/workersInShop.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  // SET ROLLING ACRES TO CHECKED
+  document.getElementById("showRA").checked = true;
+
+  //var shopLocation =$('button[#myShopChoice]:checked').val()
+  var shopLocation=$('input[name=shopChoice]:checked').val()
+  msg = "Shop location selection is " + shopLocation 
+
+  var inShop=$('input[name=inShop]:checked').val()
+  msg = msg + "\nIn Shop option selection is " + inShop 
+
+  var orderByItem = $('input[name=orderByItem]:checked').val()
+  msg = msg + "\nOrder By option selection is " + orderByItem
+
+  var filterBy=$('input[name=filterItem]:checked').val()
+  msg = msg + "\nFilter By option selection is " + filterBy;
+
+  alert(msg)
+
+  $('#myShopChoice button').click(function() {
+    //alert($(this).val())
+    var buttonValue = "No value yet"
+    buttonValue = $(this).val()
+    console.log("myShopChoice value -" + buttonValue  )
+   // $(this).addClass('active').siblings().removeClass('active');
+  })
+    /*
+$("#myShopChoice :input").change(function() {
+      console.log(this); // points to the clicked input button
+  });
+
+  $('input[name="myShopChoice"]').change( function() {
+    alert($(this).val())
+  })
+    /*
+    document.getElementById("btnRefresh").addEventListener("click", refreshList);
+  document.getElementById('BW').checked = true;
+*/
+  /*
+    function refreshList() {
+     
+      var ele = document.getElementsByName('shopChoice');        
+      for(i = 0; i < ele.length; i++) { 
+        if(ele[i].checked) 
+        shopSelected = ele[i].value; 
+      } 
+      console.log (shopSelected)
+
+      if (document.getElementById('RA').checked = true) 
+        shopNumberSelected = '1'
+      else if (document.getElementById('BW').checked = true) 
+        shopNumberSelected = '2'
+      else if (document.getElementById('BOTH').checked = true) 
+        shopNumberSelected = '3'
+      else shopNumberSelected = '0'
+
+      console.log (shopNumberSelected)
+
+      shopOptionSelected = $('button[name=shopChoice]:checked').val()
+      console.log (shopOptionSelected)
+      inShopOptionSelected = $('button[name=inShop]:checked').val()
+      orderByOptionSelected = $('button[name=orderBy]:checked').val()
+      filterOptionSelected = $('button[name=filter]:checked').val()
+      
+      msg = shopOptionSelected + inShopOptionSelected + orderByOptionSelected + filterOptionSelected
+      alert(msg)
+    )}
+*/
+})
