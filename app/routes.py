50f0ee8520bfe9736ef616afe0dbb77fb9e7966c
Richard Hartley <richardhartley@Richards-MacBook-Pro.local>
2020-08-31 15:07:36 -0400

    August 31, 2020 1507PM

--- app/routes.py
+++ app/routes.py
@@ -96,7 +96,7 @@ def stuff():
     #print ("Data - " + data)
     #return jsonify(data)
     #return jsonify(workersInShop)
-    #return render_template("workersInShop.html",workersInShop=workersInShop,shopID=shopIDcookieValue)
+    #return render_template(workersInShop.html",workersInShop=workersInShop,shopID=shopIDcookieValue)
 
 
 #@app.route("/")
@@ -115,7 +115,7 @@ def stuff():
 #     if shopID is None:
 #         return render_template("shopLocation.html")
 #     else:
-#         return redirect (url_for('workersInShop'))
+#         return redirect (url_for(':workersInShop'))
     
 
 # ORM APPROACH (FOREIGN KEY NOT WORKING)
@@ -133,15 +133,17 @@ def stuff():
 #         #     'checkIn': record.Check_In_Date_Time,
 #         #     'checkOut': record.Check_Out_Date_Time}
 #         # print(recordObject)
-#     return redirect(url_for('workersInShop'))
+#     return redirect(url_for(':workersInShop'))
 
 @app.route("/")
 @app.route("/workersInShop",methods=['GET','POST'])
 def workersInShop():
+    #print('/workersInShop request method - ', request.method)
+    
     # USING A FIXED DATE FOR TESTING
     todaysDate = datetime.date(2019,3,22)
+
     tomorrow = todaysDate + timedelta(days=1)
-    #print (tomorrow)
 
     # PROCESS POST REQUEST
     if request.method == 'POST':
@@ -182,22 +184,46 @@ def workersInShop():
                     isPresident, isVP, canSellLumber, canSellMdse, Maintenance, isBODmember, isSafetyCommittee,
                     isSpecialProjects, isAskMe
                 FROM tblMember_Activity left join tblMember_Data on tblMember_Activity.Member_ID = tblMember_Data.Member_ID""" 
-        
-        # ADD THE WHERE CLAUSE TO THE MAIN QUERY        
-        sqlCheckInRecord += whereClause
 
+        
         # REMOVE 'AND' IF IT EXISTS
         if whereClause[-4:] == 'and ':
             whereClause = whereClause[0:-5]
-        
+
+        # ADD THE WHERE CLAUSE TO THE MAIN QUERY        
+        sqlCheckInRecord += whereClause
+
         # ADD THE ORDER BY CLAUSE TO THE MAIN QUERY 
         sqlCheckInRecord += ' ' + orderBy        
         
         # EXECUTE THE SQL STATEMENT
         workersInShop = None
         workersInShop = db.engine.execute(sqlCheckInRecord)
+        workersInShopArray = []
+        workersInShopItem=''
+
+        for w in workersInShop:
+            workersInShopItem = {'name':w.memberName,
+                'checkIn':w.CheckInTime,
+                'checkOut':w.CheckOutTime,
+                'typeOfWork':w.Type_Of_Work,
+                'emergName':w.Emerg_Name,
+                'emergPhone':w.Emerg_Phone,
+                'shopNumber':w.Shop_Number,
+                'doorUsed':w.Door_Used,
+                'mentor':w.Mentor,
+                'defibrillatorTrained':w.Defibrillator_Trained,
+                'isPresident':w.isPresident,
+                'isVP':w.isVP,
+                'canSellLumber':w.canSellLumber,
+                'canSellMdse':w.canSellMdse,'maintenance':w.Maintenance,'isBODmember':w.isBODmember,'isSafetyCommittee':w.isSafetyCommittee,
+                'isSpecialProjects':w.isSpecialProjects,'isAskMe':w.isAskMe}
+            workersInShopArray.append(workersInShopItem)
         
-        return render_template("workersInShop.html",workersInShop=workersInShop,shopChoice=shopChoiceSelected,inShop=inShopSelected,orderBy=orderBySelected,filterOption=filterOptionSelected,requestMethod='POST')
+        # for w in workersInShop:
+        #     print (w.memberName,w.Member_ID,w.CheckInTime,w.CheckOutTime)
+        return render_template("workersInShop.html",workersInShopArray=workersInShopArray,shopChoice=shopChoiceSelected,\
+        inShop=inShopSelected,orderBy=orderBySelected,filterOption=filterOptionSelected,requestMethod='POST')
         
         # END OF POST REQUEST
 
@@ -215,7 +241,43 @@ def workersInShop():
     orderBy="orderByName"
     filterOption="Everyone"
     
-    return render_template("workersInShop.html",shopChoice=shopChoice,inShop=inShop,orderBy=orderBy,filterOption=filterOption,requestMethod='GET')
+    sqlCheckInRecord = """SELECT (Last_Name + ', ' +  First_Name) as memberName, tblMember_Activity.Member_ID,
+    format(Check_In_Date_Time,'hh:mm tt') as CheckInTime, Format(Check_Out_Date_Time,'hh:mm tt') as CheckOutTime,
+    Type_Of_Work, Emerg_Name, Emerg_Phone, Shop_Number, Door_Used, Mentor, Defibrillator_Trained, 
+    isPresident, isVP, canSellLumber, canSellMdse, Maintenance, isBODmember, isSafetyCommittee,
+    isSpecialProjects, isAskMe
+    FROM tblMember_Activity left join tblMember_Data on tblMember_Activity.Member_ID = tblMember_Data.Member_ID""" 
+    
+    whereClause = " WHERE Cast(Check_In_Date_Time as DATE) >= '" + str(todaysDate) + "' and Cast(Check_In_Date_Time as DATE) < '" + str(tomorrow) + "'"
+    sqlCheckInRecord += whereClause
+    
+    print (sqlCheckInRecord)
+    
+    workersInShop = None
+    workersInShop = db.engine.execute(sqlCheckInRecord)
+    workersInShopArray = []
+    workersInShopItem=''
 
+    for w in workersInShop:
+        workersInShopItem = {'name':w.memberName,
+            'checkIn':w.CheckInTime,
+            'checkOut':w.CheckOutTime,
+            'typeOfWork':w.Type_Of_Work,
+            'emergName':w.Emerg_Name,
+            'emergPhone':w.Emerg_Phone,
+            'shopNumber':w.Shop_Number,
+            'doorUsed':w.Door_Used,
+            'mentor':w.Mentor,
+            'defibrillatorTrained':w.Defibrillator_Trained,
+            'isPresident':w.isPresident,
+            'isVP':w.isVP,
+            'canSellLumber':w.canSellLumber,
+            'canSellMdse':w.canSellMdse,'maintenance':w.Maintenance,'isBODmember':w.isBODmember,'isSafetyCommittee':w.isSafetyCommittee,
+            'isSpecialProjects':w.isSpecialProjects,'isAskMe':w.isAskMe}
+        workersInShopArray.append(workersInShopItem)
+    
+    return render_template("workersInShop.html",workersInShopArray=workersInShopArray,shopChoice=shopChoice,\
+    inShop=inShop,orderBy=orderBy,filterOption=filterOption)
+    #return render_template(workersInShop.html",shopChoice=shopChoice,inShop=inShop,orderBy=orderBy,filterOption=filterOption,requestMethod='GET')
 
 
